fd6ed004ca8020fec54da5e5ec6c32d815d351e2
iemile <iemiledev@gmail.com>
2026-03-05 21:46:33 +0100

    Updating the code to use modern and clear teck

--- sensor_data.c
+++ sensor_data.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#define DEVICE_OK 1
 
-typedef struct sensor_data
+typedef enum {
+    DEVICE_ERROR = 0,
+    DEVICE_OK = 1
+} device_status_t;
+typedef struct
 {
     float humidity;
     float temperature;
@@ -11,15 +14,19 @@ typedef struct sensor_data
 void sensor_test(const sensor_data_t *data);
 
 int main(){
-    sensor_data_t data = {25.5, 60.0, DEVICE_OK};
-    sensor_data_t *ptr = &data;
-    sensor_test(ptr);
+    sensor_data_t data = {
+    .temperature = 25.5,
+    .humidity = 60.0,
+    .status = DEVICE_OK
+};
+    // sensor_data_t *ptr = &data;
+    sensor_test(&data);
     return 0;
 }
 void sensor_test(const sensor_data_t *data){
-        if(data -> status == DEVICE_OK){
-        printf("TEMPERATURE: %.2f \n", data -> temperature);
-        printf("HUMIDITY: %.2f \n", data -> humidity);
+        if(data->status == DEVICE_OK){
+        printf("TEMPERATURE: %.2f \n", data->temperature);
+        printf("HUMIDITY: %.2f \n", data->humidity);
 
     }
 }
