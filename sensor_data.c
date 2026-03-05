a144864408e615708b1d2f2e6b90fe7c34304ea3
iemile <iemiledev@gmail.com>
2026-03-05 01:32:19 +0100

    Refactor sensor struct using typedef, define and fuctions

--- sensor_data.c
+++ sensor_data.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
-struct sensor_data
+#define DEVICE_OK 1
+
+typedef struct sensor_data
 {
     float humidity;
     float temperature;
     int status;
-};
+} sensor_data_t;
+
+void sensor_test(const sensor_data_t *data);
 
 int main(){
-    struct sensor_data sensorData = {25.5, 60.0, 1};
-    struct sensor_data *ptr = &sensorData;
-
-    if(ptr->status==1){
-        printf("Temperature: %.2f \n", ptr->temperature);
-        printf("Humidity: %.2f \n", ptr->humidity);
+    sensor_data_t data = {25.5, 60.0, DEVICE_OK};
+    sensor_data_t *ptr = &data;
+    sensor_test(ptr);
+    return 0;
+}
+void sensor_test(const sensor_data_t *data){
+        if(data -> status == DEVICE_OK){
+        printf("TEMPERATURE: %.2f \n", data -> temperature);
+        printf("HUMIDITY: %.2f \n", data -> humidity);
 
     }
-    return 0;
 }
